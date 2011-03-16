3247b9ed9ea07ef78b75ac5dea9970bbf71a53bf
Thilo Brandt <mail@thilo-brandt.de>
2011-03-16 20:37:55 +0100

    [5.0.30] changed copyright info

--- kernel/core/src/ui/de/janrufmonitor/ui/jface/dialogs/InfoDialog.java
+++ kernel/core/src/ui/de/janrufmonitor/ui/jface/dialogs/InfoDialog.java
@@ -256,7 +256,7 @@ public class InfoDialog extends TitleAreaDialog {
 		
 		l = new Label(composite, SWT.RIGHT);
 		l.setText(
-			"(c) 2008 - 2010 by Thilo Brandt      "
+			"(c) 2008 - 2011 by Thilo Brandt      "
 		);
 		l.setLayoutData(gd);
 		
